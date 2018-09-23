d8476e6caab73205164ba53d02390d2281c4d4cd
GamingAuthor <league@WTS14.attlocal.net>
2018-09-23 15:59:05 -0700

    SORTS AND SEARCHES!
    wowowowowowowowu

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -1,33 +1,39 @@
 package _01_Searching_Algorithms;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
-	
+
 	@Test
 	public void testLinearSearch() {
-		//1. use the assertEquals method to test your linear search method.
+		// 1. use the assertEquals method to test your linear search method.
+		String[] words = { "lol", "hi", "hey" };
+		assertEquals(0, _00_LinearSearch.linearSearch(words, "lol"));
+		String[] hey = { "wow", "wowu", "yay", "yayu", "nice", "nicu" };
+		assertEquals(3, _00_LinearSearch.linearSearch(hey, "yayu"));
+		String[] bye = { "no", "nope", "nuhuh", "nein", "negatory" };
+		assertEquals(-1, _00_LinearSearch.linearSearch(bye, "yes"));
 	}
 
 	@Test
 	public void testBinarySearch() {
-		//2. use the assertEquals method to test your binary search method.
-		//   remember that the array must be sorted
+		// 2. use the assertEquals method to test your binary search method.
+		// remember that the array must be sorted
 	}
-	
+
 	@Test
 	public void testInterpolationSearch() {
-		//3. use the assertEquals method to test your interpolation search method.
-		//   remember that the array must be sorted and evenly distributed
+		// 3. use the assertEquals method to test your interpolation search method.
+		// remember that the array must be sorted and evenly distributed
 	}
-	
+
 	@Test
 	public void testExponentialSearch() {
-		//4. use the assertEquals method to test your exponential search method.
-		//   remember that the array must be sorted
+		// 4. use the assertEquals method to test your exponential search method.
+		// remember that the array must be sorted
 	}
 }
